6f45179f674d743d37e149fabedb28289d7e83c2
daoping.ye <daoping.ye@56qq.com>
2019-12-19 11:35:35 +0800

    初始上传

--- src/main/java/com/ydp/ez/eureka/config/WebSecurityConfig.java
+++ src/main/java/com/ydp/ez/eureka/config/WebSecurityConfig.java
@@ -11,10 +11,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
-			.authorizeRequests()
-			.antMatchers("/**").permitAll()
-			.anyRequest()
-			.authenticated().and().httpBasic();
+//		http.csrf().disable()
+//			.authorizeRequests()
+//			.antMatchers("/**").permitAll()
+//			.anyRequest()
+//			.authenticated().and().httpBasic();
 	}
 }
